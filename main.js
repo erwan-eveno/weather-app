ff904027e0bb06efa132364acde16b7b6347b741
erwaneveno <contact@erwan-eveno.fr>
2022-01-24 18:02:31 +0100

    [+] Z-index level on element click

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ const searchBar = document.querySelector(".searchBar")
 const closeBox = document.querySelector(".closeBox")
 const searchImg = document.querySelector("#searchImg")
 
+searchBar.addEventListener("click", ()=>{
+    zMax++
+    searchBar.style.zIndex = zMax
+})
+
 searchBar.addEventListener("mouseenter", ()=>{
     closeBox.style = "opacity: 1"
 })
@@ -16,6 +21,7 @@ searchBar.addEventListener("mouseleave", ()=>{
 })
 
 let isClose = false
+let zMax = 0
 
 cross.map(x=>{
     x.addEventListener("click", (e)=>{
@@ -23,18 +29,18 @@ cross.map(x=>{
             closeBox.classList.add("reduced")
             closeBox.classList.remove("maximized")
             isClose = true;
-                cityValue.style = "opacity: 0"
-                searchImg.src = "maximize.png"
-                searchBar.classList.add("searchReduced")
-                searchBar.classList.remove("searchMaximized")
-            }else{
-                isClose = false
-                closeBox.classList.add("maximized")
-                closeBox.classList.remove("reduced")
-                searchBar.classList.add("searchMaximized")
-                searchBar.classList.remove("searchReduced")
-                cityValue.style = "opacity: 1; transition: opacity 1s"
-                searchImg.src = "reduce.png"
+            cityValue.style = "opacity: 0"
+            searchImg.src = "maximize.png"
+            searchBar.classList.add("searchReduced")
+            searchBar.classList.remove("searchMaximized")
+        }else{
+            isClose = false
+            closeBox.classList.add("maximized")
+            closeBox.classList.remove("reduced")
+            searchBar.classList.add("searchMaximized")
+            searchBar.classList.remove("searchReduced")
+            cityValue.style = "opacity: 1; transition: opacity 1s"
+            searchImg.src = "reduce.png"
         }
     })
 })
@@ -43,7 +49,7 @@ const setText=(e)=>{
     let date = e.current_condition.date
     date = date.split(".")
     date = date.join("/")
-    
+
     let city = cityValue.value
     city = city.toLowerCase()
     city = city.split("")
@@ -51,14 +57,14 @@ const setText=(e)=>{
     city = city.join("")
 
     let compo = document.createElement("div")
-    compo.innerHTML = 
+    compo.innerHTML =
         `<div class="cityContainer">`+
         `<div class="cityTitle">`+
         `    <span id="cityName">${city}</span>`+
         `    <span id="cityDate">${date}</span>`+
         `</div>`+
         `<div class="cityInfos">`+
-        `    <ul>`+dada
+        `    <ul>`+
         `        <li>`+
         `            <div class="img">`+
         `                <img src="cloud.png">`+
@@ -90,20 +96,25 @@ const setText=(e)=>{
         `</div>`+
         `</div>`
 
-        compo.classList.add("dragger")
-        compo.style = "position: absolute"
+    compo.classList.add("dragger")
+    compo.style = "position: absolute"
 
-        let crossElm = compo.querySelector(".cross")
-        crossElm.addEventListener("click", (e)=>{
-            if(e.target.tagName != "IMG") e.target.parentNode.remove()
-            else e.target.parentNode.parentNode.remove()
-        })
-        compo.addEventListener("mouseenter", ()=>{
-            crossElm.style = "opacity: 1"
-        })
-        compo.addEventListener("mouseleave", ()=>{
-            crossElm.style = "opacity: 0"
-        })
+    let crossElm = compo.querySelector(".cross")
+    crossElm.addEventListener("click", (e)=>{
+        if(e.target.tagName != "IMG") e.target.parentNode.remove()
+        else e.target.parentNode.parentNode.remove()
+    })
+    compo.addEventListener("mouseenter", ()=>{
+        crossElm.style = "opacity: 1"
+    })
+    compo.addEventListener("mouseleave", ()=>{
+        crossElm.style = "opacity: 0"
+    })
+
+    compo.addEventListener("click", ()=>{
+        zMax++;
+        compo.style.zIndex = zMax
+    })
 
     document.body.appendChild(compo)
     compo.addEventListener('mousedown', e => dragStart(e, compo));
@@ -111,18 +122,18 @@ const setText=(e)=>{
 
 cityBtn.addEventListener("click", (e)=>{
     fetch(`https://www.prevision-meteo.ch/services/json/${cityValue.value}`)
-    .then(e=>e.json())
-    .then(e=>{
-        setText(e)
-        cityValue.value = ""
-    })
-    .catch(err=>{
-        dep = prompt("De quel département parlez-vous ?")
-        fetch(`https://www.prevision-meteo.ch/services/json/${cityValue.value}-${dep}`)
         .then(e=>e.json())
-        .then(e=>{setText(e);    cityValue.value = ""})
-        .catch(e=>alert("La ville n'existe pas !"))
-    })
+        .then(e=>{
+            setText(e)
+            cityValue.value = ""
+        })
+        .catch(err=>{
+            dep = prompt("De quel département parlez-vous ?")
+            fetch(`https://www.prevision-meteo.ch/services/json/${cityValue.value}-${dep}`)
+                .then(e=>e.json())
+                .then(e=>{setText(e);    cityValue.value = ""})
+                .catch(e=>alert("La ville n'existe pas !"))
+        })
 })
 
 /* Drag event */
@@ -132,7 +143,7 @@ function dragStart(e, x) {
     window.addEventListener('mousemove', drag);
     window.addEventListener('mouseup', dragEnd);
 
-    let initialX = e.clientX; 
+    let initialX = e.clientX;
     let initialY = e.clientY;
 
     function drag(e) {
@@ -140,8 +151,8 @@ function dragStart(e, x) {
         let currentY = initialY - e.clientY;
 
         const rect =  x.getBoundingClientRect();
-         x.style.left = rect.left - currentX + "px";
-         x.style.top = rect.top - currentY + "px";
+        x.style.left = rect.left - currentX + "px";
+        x.style.top = rect.top - currentY + "px";
 
         initialX = e.clientX;
         initialY = e.clientY;
